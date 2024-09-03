8c83f08587041f67def72823f2ef3980353904c2
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-09-03 09:49:27 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,87 @@
+import styled, { css } from "styled-components";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <Container>
+      <Button $variant="contained" $color="primary">
+        Default
+      </Button>
+      <Button $variant="text" $color="secondary">
+        Default
+      </Button>
+      <Button $variant="outlined" $color="secondary">
+        Default
+      </Button>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  display: flex;
+  /* flex-direction: ${function (props) {
+    if (props.$direction === undefined) {
+      return "column";
+    }
+    return props.$direction;
+  }}; */
+  /* 
+  flex-direction: ${({ $direction }) =>
+    $direction ?? "column"}; // nullish coalecsence operator */
+
+  flex-direction: ${({ $direction = "column" }) =>
+    $direction}; // nullish coalecsence operator
+
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  min-height: 100dvh;
+`;
+
+const Button = styled.button`
+  border: none;
+  cursor: pointer;
+  padding: 1rem;
+  color: white;
+  border-radius: 4px;
+  background-color: ${({ $color }) => ($color === "primary" ? "red" : "green")};
+
+  &:hover {
+    color: var(--color-primary);
+    background-color: var(--color-secondary);
+  }
+
+  ${({ $variant }) =>
+    $variant === "contained" &&
+    css`
+      background-color: var(--color-primary);
+      color: white;
+
+      &:hover {
+        color: white;
+        background-color: red;
+      }
+    `}
+
+  ${({ $variant }) =>
+    $variant === "text" &&
+    css`
+      background-color: transparent;
+      color: black;
+    `}
+
+  ${({ $variant }) => {
+    if ($variant === "outlined") {
+      return css`
+        background-color: transparent;
+        color: var(--color-primary);
+        border: 3px solid var(--color-primary);
+        font-weight: bold;
+      `;
+    }
+  }}
+`;
+
+// 3 variants
+// outlined
+// text
+// contained
--- styles.js
+++ styles.js
@@ -7,8 +7,18 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --color-primary: #364F6B;
+    --color-secondary: #3FC1C9;
+    --color-danger: #FF0000;
+  }
+
   body {
     margin: 0;
     font-family: system-ui;
+  }
+
+  main {
+
   }
 `;
